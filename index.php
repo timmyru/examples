4f4808cc339e2fb7a4c7025bf5aefda806b3a228
timmyru <33581137+timmyru@users.noreply.github.com>
2019-03-27 12:44:27 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ if (isset($_GET['text'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Палиндромы | Тестовое задание</title>
 </head>
 <body>
 <style>
