d789c4653ce7c11fa439e346da3efde820fbdf39
timmyru <33581137+timmyru@users.noreply.github.com>
2019-03-27 12:14:24 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -6,21 +6,6 @@ if (isset($_GET['text'])) {
     $text = $_GET['text'];
     $newText = str_replace(' ', '', mb_strtolower($text, 'utf-8'));
 
-    //функция, которая перебирает нашу строку и записывает в массив все комбинации букв, идущих подряд. Минимальная длина такой комбинации - три символа
-    function explodeVars($text)
-    {
-        $vars = [];
-        for ($i = 0; $i < mb_strlen($text) - 2; $i++) {
-            for ($j = 1; $j <= mb_strlen($text); $j++) {
-                if (!in_array(mb_substr($text, $i, $j), $vars)
-                    && mb_strlen(mb_substr($text, $i, $j)) > 2) {
-                    $vars[] = mb_substr($text, $i, $j);
-                }
-            }
-        }
-        return $vars;
-    }
-
     //функция, которая зеркально переворачивает наш текст
     function reverse($text)
     {
@@ -31,35 +16,52 @@ if (isset($_GET['text'])) {
         return implode($arr);
     }
 
-    //получаем все комбинации изначального и зеркального вариантов
-    $varsBeforeReverse = explodeVars($newText);
-    $varsAfterReverse = explodeVars(reverse($newText));
-
-
-    //перебираем массивы, ищем совпадения, записываем совпадения в массив. Предварительно проверяем, является ли совпадение палиндромом с помощью функции reverse
-    $match = [];
-    foreach ($varsBeforeReverse as $var) {
-        if (in_array($var, $varsAfterReverse) &&
-            $var == reverse($var)) {
-            $match[] = $var;
-        }
-    }
-
-    //ищем самое длинное совпадение, т.е. самый длинный подпалиндром
-    $longestMatch = $match[0];
-    foreach ($match as $value) {
-        if (mb_strlen($value) > mb_strlen($longestMatch)) {
-            $longestMatch = $value;
-        }
-    }
-
-    //если палиндромов не найдено, то выводим первый символ, если самый длинный подпалиндром равен исходной строке, то выводим всю исходную строку, если нет - выводим этот подпалиндром
-    if (!$longestMatch) {
-        echo 'Палиндромов и подпалиндромов не найдено. Первый символ исходной строки: ' . mb_substr($text, 0, 1);
-    } else if ($longestMatch == $newText) {
+    //если исходная строка и ее зеркальное отображение совпадают, то выводим ее на экран
+    if ($newText == reverse($newText)) {
         echo 'Строка является палиндромом, ура! ' . $text;
     } else {
-        echo 'Строка не является палиндромом, но мы нашли самый длинный подпалиндром: ' . $longestMatch;
+        //если нет, то создаем функцию, которая перебирает нашу строку и записывает в массив все комбинации букв, идущих подряд. Минимальная длина такой комбинации - три символа
+        function explodeVars($text)
+        {
+            $vars = [];
+            for ($i = 0; $i < mb_strlen($text) - 2; $i++) {
+                for ($j = 1; $j <= mb_strlen($text); $j++) {
+                    if (!in_array(mb_substr($text, $i, $j), $vars)
+                        && mb_strlen(mb_substr($text, $i, $j)) > 2) {
+                        $vars[] = mb_substr($text, $i, $j);
+                    }
+                }
+            }
+            return $vars;
+        }
+
+        //получаем все комбинации изначального и зеркального вариантов
+        $varsBeforeReverse = explodeVars($newText);
+        $varsAfterReverse = explodeVars(reverse($newText));
+
+        //перебираем массивы, ищем совпадения, записываем совпадения в массив. Предварительно проверяем, является ли совпадение палиндромом с помощью функции reverse
+        $match = [];
+        foreach ($varsBeforeReverse as $var) {
+            if (in_array($var, $varsAfterReverse) &&
+                $var == reverse($var)) {
+                $match[] = $var;
+            }
+        }
+
+        //ищем самое длинное совпадение, т.е. самый длинный подпалиндром
+        $longestMatch = $match[0];
+        foreach ($match as $value) {
+            if (mb_strlen($value) > mb_strlen($longestMatch)) {
+                $longestMatch = $value;
+            }
+        }
+
+        //если подпалиндромов не найдено, то выводим первый символ, если найдены - выводим самый длинный из них
+        if (!$longestMatch) {
+            echo 'Палиндромов и подпалиндромов не найдено. Первый символ исходной строки: ' . mb_substr($text, 0, 1);
+        } else {
+            echo 'Строка не является палиндромом, но мы нашли самый длинный подпалиндром: ' . $longestMatch;
+        }
     }
 }
 ?>
